a1c7eadc3104b95ca06bda53a7c8aac905a31092
lorenzo-cannone <lorenzo.cannone9@gmail.com>
2018-04-24 15:34:16 +0200

    edit

--- js/functions.js
+++ js/functions.js
@@ -108,7 +108,9 @@ function filterSelection(c) {
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) {
+        w3AddClass(x[i], "show");
+    }
   }
 }
 
