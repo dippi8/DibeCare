317391d9d75c2fbda1a9fecf8019e0951f0bd882
lorenzo-cannone <lorenzo.cannone9@gmail.com>
2018-06-12 14:40:04 +0200

    packages

--- index.js
+++ index.js
@@ -136,12 +136,14 @@ function initDb() {
           sqlDb.schema
             .createTable("people", table => {
               table.increments();
-              table.string("name");
+              table.string("nickname");
+              table.string("first-name");
+              table.string("last-name");
+              table.string("nickname");
               table.string("description");
               table.date("birthdate");
               table.integer("experience");
               table.integer("assisted");
-              table.string("pic-url");
               table.string("cv-url");
             })
             .then(() => {
@@ -156,4 +158,187 @@ function initDb() {
           return true;
         }
       });
+        
+    /* SERVICES - PEOPLE (WORKS) */
+    return sqlDb.schema.hasTable("works").then(exists => {
+        if (!exists) {
+          sqlDb.schema
+            .createTable("works", table => {
+              table.increments();
+              table.string("service");
+              table.string("person");
+            })
+            .then(() => {
+              return Promise.all(
+                _.map(eventList, e => {
+                  delete e.id;
+                  return sqlDb("works").insert(e);
+                })
+              );
+            });
+        } else {
+          return true;
+        }
+      });
+    
+     /* SERVICES - LOCATIONS (OFFERS) */
+    return sqlDb.schema.hasTable("offers").then(exists => {
+        if (!exists) {
+          sqlDb.schema
+            .createTable("offers", table => {
+              table.increments();
+              table.string("service");
+              table.string("location");
+            })
+            .then(() => {
+              return Promise.all(
+                _.map(eventList, e => {
+                  delete e.id;
+                  return sqlDb("offers").insert(e);
+                })
+              );
+            });
+        } else {
+          return true;
+        }
+      });
+    
+     /* EVENTS - LOCATIONS (HOSTS) */
+    return sqlDb.schema.hasTable("hosts").then(exists => {
+        if (!exists) {
+          sqlDb.schema
+            .createTable("hosts", table => {
+              table.increments();
+              table.string("events");
+              table.string("locations");
+            })
+            .then(() => {
+              return Promise.all(
+                _.map(eventList, e => {
+                  delete e.id;
+                  return sqlDb("hosts").insert(e);
+                })
+              );
+            });
+        } else {
+          return true;
+        }
+      });
+    
+     /* LOCATIONS - SLIDES (PICTURE OF) */
+    return sqlDb.schema.hasTable("picturesOf").then(exists => {
+        if (!exists) {
+          sqlDb.schema
+            .createTable("picturesOf", table => {
+              table.increments();
+              table.string("location");
+              table.string("picture");
+            })
+            .then(() => {
+              return Promise.all(
+                _.map(eventList, e => {
+                  delete e.id;
+                  return sqlDb("picturesOf").insert(e);
+                })
+              );
+            });
+        } else {
+          return true;
+        }
+      });
 }
+
+
+const _ = require("lodash");
+
+let serverPort = process.env.PORT || 5000;
+
+let servicesList = require("./servicesdata.json");
+let locationsList = require("./locationsdata.json");
+let peopleList = require("./pepoledata.json");
+let eventsList = require("./eventsdata.json");
+let servicePerson = require("./worksrel.json");
+let serviceLocation = require("./offersrel.json");
+let eventLocation = require("./hostsrel.json");
+let locationSlide = require("./picturesofrel.json");
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// /* Register REST entry point */
+app.get("/people", function(req, res) {
+  let start = parseInt(_.get(req, "query.start", 0));
+  let limit = parseInt(_.get(req, "query.limit", 5));
+  let sortby = _.get(req, "query.sort", "none");
+  let myQuery = sqlDb("people");
+
+  if (sortby === "nickname") {
+    myQuery = myQuery.orderBy("nickname", "asc");
+  } else if (sortby === "-nickname") {
+    myQuery = myQuery.orderBy("nickname", "desc");
+  }
+  myQuery
+    .limit(limit)
+    .offset(start)
+    .then(result => {
+      res.send(JSON.stringify(result));
+    });
+});
+
+// /* Register REST entry point */
+app.get("/people", function(req, res) {
+  let start = parseInt(_.get(req, "query.start", 0));
+  let limit = parseInt(_.get(req, "query.limit", 5));
+  let sortby = _.get(req, "query.sort", "none");
+  let myQuery = sqlDb("people");
+
+  if (sortby === "nickname") {
+    myQuery = myQuery.orderBy("nickname", "asc");
+  } else if (sortby === "-nickname") {
+    myQuery = myQuery.orderBy("nickname", "desc");
+  }
+  myQuery
+    .limit(limit)
+    .offset(start)
+    .then(result => {
+      res.send(JSON.stringify(result));
+    });
+});
+
+app.get("/services", function(req, res) {
+  let start = parseInt(_.get(req, "query.start", 0));
+  let limit = parseInt(_.get(req, "query.limit", 5));
+  let myQuery = sqlDb("services");
+});
+
+app.get("/locations", function(req, res) {
+  let start = parseInt(_.get(req, "query.start", 0));
+  let limit = parseInt(_.get(req, "query.limit", 5));
+  let myQuery = sqlDb("locations");
+});
+
+app.get("/events", function(req, res) {
+  let start = parseInt(_.get(req, "query.start", 0));
+  let limit = parseInt(_.get(req, "query.limit", 5));
+  let myQuery = sqlDb("events");
+});
+
+app.get("/loc-slides", function(req, res) {
+  let start = parseInt(_.get(req, "query.start", 0));
+  let limit = parseInt(_.get(req, "query.limit", 3));
+  let myQuery = sqlDb("loc-slides");
+});
+
+
+app.set("port", serverPort);
+
+initSqlDB();
+initDb();
+
+/* Start the server on port 3000 */
+app.listen(serverPort, function() {
+  console.log(`Your app is ready at port ${serverPort}`);
+});
